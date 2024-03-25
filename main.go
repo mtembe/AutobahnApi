78c9eb51b2c357b1238b093311a52cb58fe370cd
mtembe <mathieu.geipel@gmail.com>
2024-03-25 14:21:24 +0100

    redisserveradresse aktualisiert

--- main.go
+++ main.go
@@ -14,7 +14,7 @@ import (
 func main() {
 
 	client := redis.NewClient(&redis.Options{
-		Addr:     "localhost:6379",
+		Addr:     "my-release-redis-master.default.svc.cluster.local:6379",
 		Password: "",
 		DB:       0,
 	})
